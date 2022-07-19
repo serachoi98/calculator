331378de7cc53210ece6a3bf9d2c986759c0d1ba
serachoi98 <109208277+serachoi98@users.noreply.github.com>
2022-07-19 22:54:38 +0900

    first commit

---
+++ src/components/button/index.svelte
@@ -0,0 +1,35 @@
+
+
+
+<script lang="ts">
+    export let text: string;
+    export let onClick : (value: string)=> void;
+    export let color : 'orange' | 'gray' | 'dark-grey';
+</script>
+
+<div class="button"
+data-color={color}
+on:click={() => onClick(text)}>{text}</div>
+
+<style lang="scss">
+    .button {
+       width: 80px;
+       height: 80px;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+       font-size: 24px;
+
+       &[data-color='orange']{
+        background-color: rgb(193, 59, 144);
+       } &[data-color='gray']{
+        background-color: rgb(70, 57, 64);
+       }
+       &[data-color='dark-grey']{
+        background-color: darkgray;
+       }
+    }
+    </style>
+
+
+<p> This <em> Awesome Calculator </em> is made by <em>Sera Choi! </em> um_haha~!! </p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import {Button} from '@src/components/button';
+    import { text } from 'svelte/internal';
+    
+    const textNumbers: string[]= ['1','2','3','4','5','6','7','8','9','0']
+</script>
+
+<div class="frame">
+{#each textNumbers as text}
+ <Button {text} color="orange" onClick={(value) => alert(value)} />
+{/each}
+</div>
+
+<style lang="scss">
+.frame{
+    width: 240px;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+</style>
+
+<div>
+    <Button text="1" onClick={() => {}} color="orange" />
+</div>
